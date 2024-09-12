dfc3a8c7cad58dda5f0c347c4c1e3f96e91bcecf
Jolin Qiu <qiujolin1@gmail.com>
2024-09-12 17:17:35 -0400

    colon constructor

--- src/tokens/Colon.java
+++ src/tokens/Colon.java
@@ -1,5 +1,21 @@
 package tokens;
+import provided.*;
 
 public class Colon {
+
+    public final static TokenType TOKEN = TokenType.COLON;
+    private Token colon;
+
+    /**
+     * Creates Colon Token
+     * @param filename the name of the file the token came from
+     * @param lineNum the number of the line in the file that the token appears on
+     * @return the token
+     */
+    public Colon(String filename, int lineNumber)
+    {
+        this.colon = new Token(":", filename, lineNumber,TOKEN);
+    }
     
 }
+
