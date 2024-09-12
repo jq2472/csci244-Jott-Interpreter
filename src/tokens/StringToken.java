e22b970ffc097a71249088abc58d4a78822c3774
Jolin Qiu <qiujolin1@gmail.com>
2024-09-12 18:57:22 -0400

    rmv StringToken

--- src/tokens/StringToken.java
+++ src/tokens/StringToken.java
@@ -1,17 +1,5 @@
 package tokens;
 
 public class StringToken {
-    private String strtoken;
-    public StringToken()
-    {
-        this.strtoken="";
-    }
-    public String getstringtoken()
-    {
-        return this.strtoken;
-    }
-    public void setstringtoken(String str)
-    {
-        this.strtoken = str;
-    }
+    
 }
