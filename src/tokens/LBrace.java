131943c2d1f098acdabd4e013b651bb83660d8c1
Jolin Qiu <qiujolin1@gmail.com>
2024-09-12 17:49:41 -0400

    LBrace class modified

--- src/tokens/LBrace.java
+++ src/tokens/LBrace.java
@@ -1,18 +1,20 @@
 package tokens;
 
-public class LBrace {
-    private String lbrace;
-    public LBrace()
+import provided.*;
+
+public class LBrace extends Token {
+
+    public final static TokenType TOKEN = TokenType.L_BRACE;
+
+    /**
+     * Creates LBrace Token
+     * @param filename the name of the file the token came from
+     * @param lineNum the number of the line in the file that the token appears on
+     * @return the token
+     */
+    public LBrace(String filename, int lineNumber)
     {
-        this.lbrace = "";
-    }
-    public String getlbrace()
-    {
-        return this.lbrace;
-    }
-    public void setlbrace(String lbrace)
-    {
-        this.lbrace = lbrace;
+        super("{", filename, lineNumber,TOKEN);
     }
     
 }
