50850dd3b79327836f4c932181b5e654a9d8d8da
Jolin Qiu <qiujolin1@gmail.com>
2024-09-12 19:22:26 -0400

    charstream edits

--- src/tokens/RelOp.java
+++ src/tokens/RelOp.java
@@ -7,13 +7,14 @@ public class RelOp extends Token {
 
     /**
      * Creates RelOp Token
+     * @param charStream the stream of necessary scanned characters
      * @param filename the name of the file the token came from
      * @param lineNum the number of the line in the file that the token appears on
      * @return the token
      */
-    public RelOp(String filename, int lineNumber)
+    public RelOp(String charStream, String filename, int lineNumber)
     {
-        super("<>", filename, lineNumber,TOKEN);
+        super(charStream, filename, lineNumber,TOKEN);
     }
     
 }
