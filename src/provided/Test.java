a3ce9dc3683a48295d6ae0d34df755f9cfb20ae6
kxh8353 <kxh8353@rit.edu>
2024-09-20 16:11:36 -0400

    pulling from main

--- src/provided/Test.java
+++ src/provided/Test.java
@@ -1,7 +1,7 @@
-package provided;
+// package provided;
 
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("here");
-    }
-}
+// public class Test {
+//     public static void main(String[] args) {
+//         System.out.println("here");
+//     }
+// }
