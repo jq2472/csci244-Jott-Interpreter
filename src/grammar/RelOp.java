891e264357b2db3b87a1e671485e19d3cbdb618d
kxh8353 <kxh8353@rit.edu>
2024-10-08 17:39:39 -0400

    implemented RelOp.java

--- src/grammar/RelOp.java
+++ src/grammar/RelOp.java
@@ -1,5 +1,36 @@
 package grammar;
+import java.util.ArrayList;
+import provided.*;
+import static grammar.Helper.*; // checkTokenType(), checkIsNotEmpty()
 
-public class RelOp {
+public class RelOp implements ExprNode{
+
+    private Token RelOpToken;
+
+    public RelOp(Token token){
+        this.RelOpToken = token;
+    }
     
+    public static RelOp parseRelOpNode(ArrayList<Token> tokens){
+        checkIsNotEmpty(tokens);
+        checkTokenType(tokens, TokenType.MATH_OP);
+        RelOp RelOpToken = new RelOp(tokens.get(0));
+        tokens.remove(0);
+        return RelOpToken;
+    }
+
+    @Override
+    public String convertToJott(){
+        return "" + this.RelOpToken.getToken();
+    }
+
+    @Override
+    public boolean validateTree(){
+        return true;
+    }
+
+    @Override
+    public void execute(){
+        System.out.println("executing RelOp");
+    }
 }
