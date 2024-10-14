d597be1ffaf9af79acf13b10a16bcf2c3ab89abe
James <jm1060@rit.edu>
2024-10-14 11:46:09 -0400

    Made Function_RetNode, could use some work but it follows the basic concept.

---
+++ src/grammar/Function_RetNode.java
@@ -0,0 +1,66 @@
+package grammar;
+import provided.*;
+import java.util.ArrayList;
+
+import static grammar.Helper.*; // checkTokenType(), checkIsNotEmpty()
+public class Function_RetNode implements JottTree{
+    private TypeNode typeNode;
+    private Token voidtoken;
+    public Function_RetNode(TypeNode typeNode)
+    {
+        this.typeNode = typeNode;
+        this.voidtoken = null;
+    }
+    public Function_RetNode(Token voidToken)
+    {
+        this.typeNode = null;
+        this.voidtoken = voidToken;
+    }
+    
+    public Function_RetNode parsfunctionRetNode(ArrayList<Token>tokens)
+    {
+        checkIsNotEmpty(tokens);
+        checkTokenType(tokens, TokenType.ID_KEYWORD);
+        Token currToken = tokens.get(0);
+        if(currToken.getToken().equals("Void"))
+        {
+            tokens.remove(0);
+            return new Function_RetNode(currToken);
+        }
+        else if(currToken.getToken().equals("Double") || currToken.getToken().equals("String") 
+        || currToken.getToken().equals("Integer") || currToken.getToken().equals("Boolean"))
+        {
+            return new Function_RetNode(TypeNode.parseTypeNode(tokens));
+        }
+        else 
+        {
+            throw new IllegalArgumentException(ERROR_MESSAGE + ", Got ID_KEYWORD, but not one of the Keywords for  node");
+        }
+
+        
+    }
+    @Override
+    public String convertToJott() {
+        String returnedString = "";
+        if(this.typeNode == null)
+        {
+            returnedString =this.voidtoken.getToken();
+        }
+        else
+        {
+            returnedString = this.typeNode.convertToJott();
+        }
+        return returnedString;
+    }
+
+    @Override
+    public boolean validateTree() {
+        return true;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("Function_RetNode");
+    }
+    
+}
