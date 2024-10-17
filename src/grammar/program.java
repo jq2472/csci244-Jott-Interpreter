fe56bab6ca52dfb5bff8263b3f196f105764a0b8
wolfgrey139 <alexdenny25@gmail.com>
2024-10-17 18:24:19 -0400

    Added methods to Program

---
+++ src/grammar/program.java
@@ -0,0 +1,37 @@
+package grammar;
+
+import provided.*;
+
+import java.util.ArrayList;
+
+import static grammar.Helper.*; // checkTokenType(), checkIsNotEmpty()
+
+
+public class Program implements JottTree{
+
+    public static void main(ArrayList<Token> tokens){
+
+        while(checkIsNotEmpty(tokens)){
+            
+        }
+    }
+
+    @Override
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+
+}
