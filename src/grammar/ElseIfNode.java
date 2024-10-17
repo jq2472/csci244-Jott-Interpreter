aa45e7fbb9d68483b3ef251b64ab2ca5fd1622d2
kxh8353 <kxh8353@rit.edu>
2024-10-17 18:09:53 -0400

    edited ElseIfNode

--- src/grammar/ElseIfNode.java
+++ src/grammar/ElseIfNode.java
@@ -8,11 +8,15 @@ import java.util.ArrayList;
 public class ElseIfNode implements JottTree, ExprNode{
 
     private ArrayList<JottTree>bodyNode;
+    private ArrayList<ExprNode>exprNode;
 
-    public ElseIfNode(ArrayList<JottTree>bodyNode)
-    {
+    public ElseIfNode(ArrayList<JottTree>bodyNode, ArrayList<ExprNode>exprNode){
         this.bodyNode = bodyNode;
+        this.exprNode = exprNode;
     }
+
+   
+
 
 
     public ElseIfNode ElseIfNode(ArrayList<Token>tokens) throws Exception{
@@ -23,11 +27,10 @@ public class ElseIfNode implements JottTree, ExprNode{
         if(tokens.get(0).getToken().equals("elseif")){
             tokens.remove(0);
             bodyTree = BodyNode.parsebodynode(tokens);
-            return new ElseIfNode(bodyTree);
+            return new ElseIfNode(bodyTree, exprNode);
         }else{
-            return new ElseIfNode(null);
+            return new ElseIfNode(null, exprNode);
         }
-
     }
 
     @Override
