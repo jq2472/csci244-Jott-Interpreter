d7362dff430aaa35a8e32e19945cb57585d8542c
wolfgrey139 <alexdenny25@gmail.com>
2024-10-24 17:28:48 -0400

    Readded all test cases

--- src/testers/JottParserTester.java
+++ src/testers/JottParserTester.java
@@ -27,15 +27,15 @@ public class JottParserTester {
     }
 
     private void createTestCases(){
-        this.testCases = new ArrayList<>(); /**
+        this.testCases = new ArrayList<>(); 
         testCases.add(new TestCase("provided writeup example1", "providedExample1.jott", false ));
         testCases.add(new TestCase("provided writeup example2 (error)", "providedExample2.jott", true ));
         testCases.add(new TestCase("provided writeup example3 (error)", "providedExample3.jott", true ));
         testCases.add(new TestCase("provided writeup example4 (error)", "providedExample4.jott", true ));
         testCases.add(new TestCase("provided writeup example5 (error)", "providedExample5.jott", true ));
         testCases.add(new TestCase("hello world", "helloWorld.jott", false ));
-        testCases.add(new TestCase("1foo error (error)", "1foo.jott", true )); */
-        testCases.add(new TestCase("return <id> type mismatch", "returnId.jott", false ));/**
+        testCases.add(new TestCase("1foo error (error)", "1foo.jott", true )); 
+        testCases.add(new TestCase("return <id> type mismatch", "returnId.jott", false ));
         testCases.add(new TestCase("type:var error (error)", "paramOrderSwapped.jott", true ));
         testCases.add(new TestCase("missing expr (error)", "missingExp.jott", true ));
         testCases.add(new TestCase("missingBrace (error)", "missingBrace.jott", true ));
@@ -58,7 +58,7 @@ public class JottParserTester {
         //testCases.add(new TestCase("lone minus (error)", "loneMinus.jott", true ));
         testCases.add(new TestCase("else without if (error)", "elseNoIf.jott", true ));
         testCases.add(new TestCase("missing closing } (error)", "missingClosing.jott", true ));
-        testCases.add(new TestCase("valid if with return", "validIfReturn.jott", false)); */
+        testCases.add(new TestCase("valid if with return", "validIfReturn.jott", false));
     }
 
     private boolean parserTest(TestCase test, String orginalJottCode){
